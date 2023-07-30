8c122d0da82b015750754db3bb26ef9c6b0d1d8d
saidaDelRosario <saidadr@gmail.com>
2023-07-29 22:57:52 -0500

    Cleaned up code

--- src/index.js
+++ src/index.js
@@ -32,8 +32,59 @@ presentDateTime.innerHTML = `${day}, ${month} ${date} ${year}, ${hours}:${minute
 
 //Feature 2 Add a search engine, when searching for a city (i.e. Paris), display the city name on the page after the user submits the form.
 
+function showCity(event) {
+  event.preventDefault();
+  let showCityInput = document.querySelector("#city-input");
+  let currentCity = document.querySelector("#city-name");
+  currentCity.innerHTML = showCityInput.value.toUpperCase();
+  console.log(showCityInput.value);
+
+  let city = showCityInput.value;
+  let apiKey = "ec23e2ff6f0483966bf50ed682b76bdd";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+  function showCityTemp(response) {
+    let searchCityTemp = Math.round(response.data.main.temp);
+    let cityTemp = document.querySelector("#display-current-temp");
+    cityTemp.innerHTML = `${searchCityTemp}`;
+
+    document.querySelector("#weather-description").innerHTML =
+      response.data.weather[0].description.toUpperCase();
+
+    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+
+    document.querySelector("#wind").innerHTML = Math.round(
+      response.data.wind.speed
+    );
+  }
+
+  axios.get(apiUrl).then(showCityTemp);
+}
+
 let citySearch = document.querySelector("#city-search-form");
 citySearch.addEventListener("submit", showCity);
+
+// Celcius Display
+
+//function displayCelsius(event) {
+//event.preventDefault();
+
+//let showCityTempCelsius = document.querySelector("#display-current-temp");
+//showCityTempCelsius.innerHTML = "0";}
+
+//let tempCelsius = document.querySelector("#temp-celsius");
+//tempCelsius.addEventListener("click", displayCelsius);
+
+//Fahrenheit Display
+
+//function displayFahrenheit(event) {
+//event.preventDefault();
+
+//let showCityTempFahrenheit = document.querySelector("#display-current-temp");
+//showCityTempFahrenheit.innerHTML = "0";}
+
+//let tempFahrenheit = document.querySelector("#temp-fahrenheit");
+//tempFahrenheit.addEventListener("click", displayFahrenheit);
 
 //BONUS
 
@@ -71,24 +122,3 @@ function showCurrentCity() {
 
 let currentLocation = document.querySelector("#current-city-button");
 currentLocation.addEventListener("click", showCurrentCity);
-
-//Fahrenheit Display
-
-//function displayFahrenheit(event) {event.preventDefault();
-
-//let showCityTempFahrenheit = document.querySelector("#display-current-temp");
-//showCityTempFahrenheit.innerHTML = "0";}
-
-//let tempFahrenheit = document.querySelector("#temp-fahrenheit");
-//tempFahrenheit.addEventListener("click", displayFahrenheit);
-
-// Celcius Display
-
-//function displayCelsius(event) {
-//event.preventDefault();
-
-//let showCityTempCelsius = document.querySelector("#display-current-temp");
-//showCityTempCelsius.innerHTML = "1";}
-
-//let tempCelsius = document.querySelector("#temp-celsius");
-//tempCelsius.addEventListener("click", displayCelsius);
