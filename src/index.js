9b9c0b9022c9515b16a07f96919e10c97ed57f4e
saidaDelRosario <saidadr@gmail.com>
2023-08-03 22:53:42 -0500

    Injected Foreccast HTML in JS and multiplied the columns using JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
+//SHOW CURRENT LOCATION TEMP/WEATHER CONDITIONS ON PAGE LOAD
+let body = document.querySelector("body");
+
+function showCurrentCity() {
+  function showCityTemp(response) {
+    console.log(response.data.main.temp);
+    let currentCityTemp = Math.round(response.data.main.temp);
+
+    FahrenheitTemp = Math.round(response.data.main.temp);
+
+    let currentCityName = response.data.name.toUpperCase();
+
+    let currentCityConditions =
+      response.data.weather[0].description.toUpperCase();
+
+    let displayTemp = document.querySelector("#display-current-temp");
+    displayTemp.innerHTML = `${currentCityTemp}`;
+
+    let displayCityName = document.querySelector("#city-name");
+    displayCityName.innerHTML = `${currentCityName}`;
+
+    let displayCityConditions = document.querySelector("#weather-description");
+    displayCityConditions.innerHTML = `${currentCityConditions}`;
+
+    let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute(
+      "src",
+      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  }
+
+  function showCurrentPostion(position) {
+    let lat = position.coords.latitude;
+    console.log(lat);
+    let lon = position.coords.longitude;
+    console.log(lon);
+
+    let apiKey = "4f97a6af475a359f35870845c4249adb";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+
+    axios.get(apiUrl).then(showCityTemp);
+  }
+
+  navigator.geolocation.getCurrentPosition(showCurrentPostion);
+}
+
 //SEARCH CITY WEATHER
 
 let presentDate = new Date();
@@ -34,15 +80,20 @@ presentDateTime.innerHTML = `${day}, ${month} ${date} ${year}, ${hours}:${minute
 //Forecast display
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = `<div class="row">
-        <div class="col-2">
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
           <div class="card" style="width: 7rem">
             <img
               src="images/cloudSun.jpg"
               class="card-img-top"
               alt="cloudSun" />
             <div class="card-body">
-              <h5 class="card-title text-center">SAT</h5>
+              <h5 class="card-title text-center">${day}</h5>
               <p class="card-text text-center">
                 <span class="max-temp">98°</span
                 ><span class="min-temp"> 77° </span>
@@ -50,6 +101,10 @@ function displayForecast() {
             </div>
           </div>
        </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 //Feature 2 Add a search engine, when searching for a city (i.e. Paris), display the city name on the page after the user submits the form.
@@ -122,52 +177,6 @@ function displayFahrenheit(event) {
 
 let tempFahrenheit = document.querySelector("#temp-fahrenheit");
 tempFahrenheit.addEventListener("click", displayFahrenheit);
-
-//SHOW CURRENT LOCATION TEMP/WEATHER CONDITIONS ON PAGE LOAD
-let body = document.querySelector("body");
-
-function showCurrentCity() {
-  function showCityTemp(response) {
-    console.log(response.data.main.temp);
-    let currentCityTemp = Math.round(response.data.main.temp);
-
-    FahrenheitTemp = Math.round(response.data.main.temp);
-
-    let currentCityName = response.data.name.toUpperCase();
-
-    let currentCityConditions =
-      response.data.weather[0].description.toUpperCase();
-
-    let displayTemp = document.querySelector("#display-current-temp");
-    displayTemp.innerHTML = `${currentCityTemp}`;
-
-    let displayCityName = document.querySelector("#city-name");
-    displayCityName.innerHTML = `${currentCityName}`;
-
-    let displayCityConditions = document.querySelector("#weather-description");
-    displayCityConditions.innerHTML = `${currentCityConditions}`;
-
-    let iconElement = document.querySelector("#icon");
-    iconElement.setAttribute(
-      "src",
-      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
-  }
-
-  function showCurrentPostion(position) {
-    let lat = position.coords.latitude;
-    console.log(lat);
-    let lon = position.coords.longitude;
-    console.log(lon);
-
-    let apiKey = "4f97a6af475a359f35870845c4249adb";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
-
-    axios.get(apiUrl).then(showCityTemp);
-  }
-
-  navigator.geolocation.getCurrentPosition(showCurrentPostion);
-}
 
 //let currentLocation = document.querySelector("#current-city-button");
 //currentLocation.addEventListener("click", showCurrentCity);
